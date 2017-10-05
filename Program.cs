1669601df9337f37033f69a94c07ff7806557d4d
GRANTADESIGN\sergio.trillo <senrech@yahoo.fr>
2017-10-05 16:01:38 +0100

    first proof of concept

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyInDotNet
+{
+    class Program
+    {
+        public static string LoadJson(string file)
+        {
+            using (StreamReader r = new StreamReader(@"D:\Temp\" + file))
+            {
+                string payload = r.ReadToEnd();
+
+                return payload;
+            }
+        }
+
+        static void Main()
+        {
+            string jsonFile = LoadJson("license.lic");
+            string tamperedFile = LoadJson("tampered.lic");
+
+            var docTampered = Encoding.UTF8.GetBytes(tamperedFile);
+            var document = Encoding.UTF8.GetBytes(jsonFile);
+
+            byte[] hashedDocument;
+            byte[] tamperedHashedDocument;
+
+            using (var sha256 = SHA256.Create())
+            {
+                hashedDocument = sha256.ComputeHash(document);
+            }
+
+            using (var sha256 = SHA256.Create())
+            {
+                tamperedHashedDocument = sha256.ComputeHash(docTampered);
+            }
+
+            // setup keys
+            var storeKey = new StoreKeyLocal("SergioBag");
+            var digitalSignature = new DigitalSignature(storeKey);
+
+
+            digitalSignature.UseContainer();
+
+            var signature = digitalSignature.SignData(hashedDocument);
+            var verified = digitalSignature.VerifySignature(hashedDocument, signature);
+            var verifiedTampered = digitalSignature.VerifySignature(tamperedHashedDocument, signature);
+            
+            Console.WriteLine("Digital Signature Demonstration in .NET");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine();            
+            Console.WriteLine();
+            Console.WriteLine("   Original Text = " + 
+                Encoding.Default.GetString(document));
+
+            Console.WriteLine();
+            Console.WriteLine("   Digital Signature = " + 
+                Convert.ToBase64String(signature));
+            Console.WriteLine();
+            Console.WriteLine("   Hash original Doc= " +
+                              Convert.ToBase64String(hashedDocument));
+            Console.WriteLine("   Hash tampered Doc= " +
+                              Convert.ToBase64String(tamperedHashedDocument));
+            Console.WriteLine();
+
+            Console.WriteLine(verified
+                ? "The digital signature has been correctly verified."
+                : "The digital signature has NOT been correctly verified.");
+
+            Console.WriteLine("Tampered document");
+            Console.WriteLine(verifiedTampered
+                ? "The digital signature has been correctly verified."
+                : "The digital signature has NOT been correctly verified.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StoreKeyLocal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace CryptographyInDotNet
+{
+    public class StoreKeyLocal
+    {
+        private string containerName;
+        public StoreKeyLocal(string containerName)
+        {
+            this.containerName = containerName;
+        }
+        public void GenKey_SaveInContainer()
+        {
+            // Create the CspParameters object and set the key container   
+            // name used to store the RSA key pair.  
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = containerName;
+
+            // Create a new instance of RSACryptoServiceProvider that accesses  
+            // the key container MyKeyContainerName.  
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
+
+            // Display the key information to the console.  
+            Console.WriteLine("Key added to container: \n  {0}", rsa.ToXmlString(true));
+        }
+
+        public void GetKeyFromContainer()
+        {
+            // Create the CspParameters object and set the key container   
+            // name used to store the RSA key pair.  
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = containerName;
+
+            // Create a new instance of RSACryptoServiceProvider that accesses  
+            // the key container MyKeyContainerName.  
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
+
+            // Display the key information to the console.  
+            Console.WriteLine("Key retrieved from container : \n {0}", rsa.ToXmlString(true));
+        }
+
+        public RSAParameters GetPublicKey()
+        {
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = containerName;
+
+            var rsa = new RSACryptoServiceProvider(cp);
+
+            return rsa.ExportParameters(false);
+        }
+
+        public RSAParameters GetPrivateKey()
+        {
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = containerName;
+
+            var rsa = new RSACryptoServiceProvider(cp);
+
+            return rsa.ExportParameters(true);
+        }
+
+        public void DeleteKeyFromContainer()
+        {
+            // Create the CspParameters object and set the key container   
+            // name used to store the RSA key pair.  
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = containerName;
+
+            // Create a new instance of RSACryptoServiceProvider that accesses  
+            // the key container.  
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
+
+            // Delete the key entry in the container.  
+            rsa.PersistKeyInCsp = false;
+
+            // Call Clear to release resources and delete the key from the container.  
+            rsa.Clear();
+
+            Console.WriteLine("Key deleted.");
+        }
+    }
+}
